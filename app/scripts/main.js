522ae93d7bb2051299405bc804b7d1108afc9e43
Gabrijela Vidaković <gabrijela.vidakovic@burza.hr>
2017-09-24 02:20:28 +0200

    feature(section): Feeder section 4 style, masonry gallery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('\'Allo \'Allo!');
+
+
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+// init Masonry
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
+});
+$(document).ready(function() {
+
+  $('#design').click(function() {
+    $('#design-content').show().removeClass('hidden');
+    $('#identity-content').addClass('hidden').hide();
+    $('#web-design-content').addClass('hidden').hide();
+    $('#photography-content').addClass('hidden').hide();
+  });
+
+  $('#photography').click(function() {
+    $('#photography-content').show().removeClass('hidden');
+    $('#identity-content').addClass('hidden').hide();
+    $('#web-design-content').addClass('hidden').hide();
+    $('#design-content').addClass('hidden').hide();
+  });
+
+  $('#identity').click(function() {
+    $('#identity-content').show().removeClass('hidden');
+    $('#photography-content').addClass('hidden').hide();
+    $('#web-design-content').addClass('hidden').hide();
+    $('#design-content').addClass('hidden').hide();
+  });
+
+  $('#web-design').click(function() {
+    $('#web-design-content').show().removeClass('hidden');
+    $('#photography-content').addClass('hidden').hide();
+    $('#identity-content').addClass('hidden').hide();
+    $('#design-content').addClass('hidden').hide();
+  });
+
+  $('#show-all').click(function() {
+    $('.grid div').removeClass('hidden');
+
+  });
+});
